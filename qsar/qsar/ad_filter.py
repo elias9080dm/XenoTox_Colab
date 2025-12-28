73fb07e803e5dc9f810b06a7f99a65906b5434be
elias9080dm <elias9080dm@gmail.com>
2025-12-28 12:48:41 -0600

    Delete qsar/qsar directory

--- qsar/qsar/ad_filter.py
+++
@@ -1,8 +0,0 @@
-def filter_by_ad(df):
-    """
-    Filtra compuestos activos dentro del dominio de aplicabilidad
-    """
-    return df[
-        (df["Prediction"] == "Active") &
-        (df["AD_Flag"] == "IN")
-    ].copy()
