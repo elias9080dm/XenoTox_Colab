0a8e24a3ee0249c384b4dd8b53f5a0771761f843
elias9080dm <elias9080dm@gmail.com>
2026-03-04 19:03:58 -0600

    Delete qsar directory

--- qsar/utils.py
+++
@@ -1,8 +0,0 @@
-# Cleaning function 
-import numpy as np
-def clean_numeric(X, clip_value=1e4):
-    # Reemplazar NaN, inf, -inf
-    X = np.nan_to_num(X, nan=0.0, posinf=clip_value, neginf=-clip_value)
-    # Clip valores extremos
-    X = np.clip(X, -clip_value, clip_value)
-    return X
